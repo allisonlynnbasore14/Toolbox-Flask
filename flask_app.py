eb1733ead60c3fa2183a09164a781060dee736f4
allisonlynnbasore14 <allison.basore@students.olin.edu>
2017-03-28 21:03:58 -0400

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,28 @@
 """
-Put your Flask app code here.
+Flask Code.
 """
+
+
+from flask import Flask, request, render_template, url_for, flash, redirect
+app = Flask(__name__)
+
+@app.route('/profile', methods = ['GET', 'POST'])
+def profile():
+
+	if request.method == 'POST':
+		age = request.form['age']
+		firstname = request.form['firstname']
+		lastname = request.form['lastname']
+		Ninja = request.form['Ninja']
+		if age == '' or firstname == '' or lastname == '' or Ninja == '':
+			return render_template("profileERROR.html")
+		return render_template("profile.html", age= age, firstname = firstname, lastname = lastname, Ninja = Ninja)
+	
+
+	return render_template("Toolbox.html")
+
+
+
+
+if __name__ == "__main__":
+	app.run()
