e178c59e183b4ab0f83cde02d91675e9a486849a
allisonlynnbasore14 <allison.basore@students.olin.edu>
2017-03-28 21:04:28 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,8 +6,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+	return 'Index page'
+    #return render_template('index.html')
+
+@app.route('/tuna')
+def tuna():
+	return '<h2> Tuna is good<h2>'
+
+@app.route('/profile/<username>')
+def profile(username):
+	return 'Hey there %s' %username
+
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
